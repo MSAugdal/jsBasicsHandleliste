26433a6a7006b2599ace094feccd9374dd8f6a4d
MSAugdal <mathias.s.augdal@gmail.com>
2024-04-17 15:49:49 +0200

    💡 Add comments and hover effect
    added comments to source code, and finished implementing the changing of emoji on handleliste item on hover

--- src/main.js
+++ src/main.js
@@ -1,17 +1,22 @@
+// selectors for list div, submit button and input field.
 const list = document.querySelector("#list");
 const submit = document.querySelector("#submit");
 const input = document.querySelector("#input");
 
+// initializes array to hold all handleliste items,
+// and id which increments for every new handleliste item, and
+// gets assigned to the handeliste item as it´s id.
 let listArray = [];
 let id = 0;
 
+// function to generate handleliste items, add eventlisteners, and append it to listArray
 function appendItem(name) {
   if (name.length < 1) return;
   const listItem = document.createElement("div");
   listItem.id = id;
   listItem.classList += "listitem hoverItem flex flex-row gap-[1rem] bg-white p-4 w-64 rounded";
   listItem.innerHTML = `
-<p id="${id}" class="${id} show pointer-events-none">🫣</p>
+<p id="${id}" class="${id} show pointer-events-none">🎁</p>
     <p id="${id}" class=" pointer-events-none">${name}</p>
     `;
 
@@ -20,13 +25,12 @@ function appendItem(name) {
   });
   listItem.addEventListener("mouseover", (e) => {
     if (e.target.classList.contains("listitem")) {
-      e.target.firstChild.innerHTML = "❌";
+      e.target.children[0].innerHTML = `❌`;
     }
   });
   listItem.addEventListener("mouseout", (e) => {
     if (e.target.classList.contains("listitem")) {
-      e.target.firstChild;
-      console.log(e.target.firstChild);
+      e.target.children[0].innerHTML = `🎁`;
     }
   });
 
@@ -35,18 +39,20 @@ function appendItem(name) {
   id += 1;
 }
 
+// removes the handleliste item with specified id
 function removeItem(id) {
   list.removeChild(listArray[id]);
   listArray.filter((item) => item.id == id ? true : false);
 }
 
-
+// eventlistener for input field and submit button.
+// creates handleliste item on Enter for input field,
+// and on click for submit button
 input.addEventListener("keyup", (e) => {
   if (e.key == 'Enter' || e.keycode == 13) {
     appendItem(input.value);
   }
 });
-
 submit.addEventListener("click", () => {
   appendItem(input.value);
 });
