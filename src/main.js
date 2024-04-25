483a0620bd23eda03ba05b1cc80c0426c15cb51e
MSAugdal <mathias.s.augdal@gmail.com>
2024-04-25 11:18:42 +0200

    ♻️ Complete refactor
    complete refactor of codebase

--- src/main.js
+++ src/main.js
@@ -1,58 +1,79 @@
-// selectors for list div, submit button and input field.
-const list = document.querySelector("#list");
-const submit = document.querySelector("#submit");
 const input = document.querySelector("#input");
+const button = document.querySelector("#button");
+const list = document.querySelector("#list");
 
-// initializes array to hold all handleliste items,
-// and id which increments for every new handleliste item, and
-// gets assigned to the handeliste item as it´s id.
-let listArray = [];
-let id = 0;
+function onSubmitButtonClick() {
+  const newItem = input.value.trim();
+  const items = getListItemsFromStorage();
 
-// function to generate handleliste items, add eventlisteners, and append it to listArray
-function appendItem(name) {
-  if (name.length < 1) return;
-  const listItem = document.createElement("div");
-  listItem.id = id;
-  listItem.classList += "listitem hoverItem flex flex-row gap-[1rem] bg-white p-4 w-64 rounded";
-  listItem.innerHTML = `
-<p id="${id}" class="${id} show pointer-events-none">🎁</p>
-    <p id="${id}" class=" pointer-events-none">${name}</p>
-    `;
-
-  listItem.addEventListener("click", (e) => {
-    removeItem(e.target.id);
-  });
-  listItem.addEventListener("mouseover", (e) => {
-    if (e.target.classList.contains("listitem")) {
-      e.target.children[0].innerHTML = `❌`;
-    }
-  });
-  listItem.addEventListener("mouseout", (e) => {
-    if (e.target.classList.contains("listitem")) {
-      e.target.children[0].innerHTML = `🎁`;
-    }
-  });
-
-  listArray.push(listItem);
-  list.appendChild(listItem);
-  id += 1;
-}
-
-// removes the handleliste item with specified id
-function removeItem(id) {
-  list.removeChild(listArray[id]);
-  listArray.filter((item) => item.id == id ? true : false);
-}
-
-// eventlistener for input field and submit button.
-// creates handleliste item on Enter for input field,
-// and on click for submit button
-input.addEventListener("keyup", (e) => {
-  if (e.key == 'Enter' || e.keycode == 13) {
-    appendItem(input.value);
+  if (items.includes(newItem)) {
+    alert("Item already exists!");
+    return;
   }
-});
-submit.addEventListener("click", () => {
-  appendItem(input.value);
-});
+
+  addListItemToStorage(newItem);
+  addListItemToDOM(newItem);
+}
+
+function onRemoveButtonClick(e) {
+  const targetItem = e.target.parentElement;
+  list.removeChild(targetItem);
+  removeItemFromStorage(targetItem.id);
+}
+
+function getListItemsFromStorage() {
+  let items;
+  if (localStorage.getItem("items") === null) items = [];
+  else items = JSON.parse(localStorage.getItem("items"));
+  return items;
+}
+
+function addListItemToStorage(item) {
+  const items = getListItemsFromStorage();
+  items.push(item);
+  localStorage.setItem("items", JSON.stringify(items));
+}
+
+function removeItemFromStorage(item) {
+  let items = getListItemsFromStorage();
+  const index = items.indexOf(item);
+  items.splice(index, 1);
+
+  localStorage.setItem("items", JSON.stringify(items));
+}
+
+
+function addListItemToDOM(item) {
+  const newItem = createListItem(item);
+  list.appendChild(newItem);
+}
+
+function createButton() {
+  const _button = document.createElement("button");
+  _button.addEventListener("click", onRemoveButtonClick);
+  _button.classList += "bg-white text-black text-sm ml-1 px-1 rounded";
+  _button.innerText = "Remove";
+  return _button;
+}
+
+function createListItem(itemName) {
+  const item = document.createElement("li");
+  item.classList += "bg-gray-900 flex gap-4 text-white text-lg px-6 py-2 rounded";
+  item.id = itemName;
+  item.innerText = itemName;
+  item.append(createButton());
+
+  return item;
+}
+
+
+function initialize() {
+  button.addEventListener("click", onSubmitButtonClick);
+
+  const items = getListItemsFromStorage();
+  items.forEach((item) => {
+    addListItemToDOM(item);
+  });
+}
+
+initialize();
